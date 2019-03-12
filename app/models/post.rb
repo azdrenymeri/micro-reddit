807fe7258dd9a259fe674cb256b85701327b889a
Azdren <azdreny@gmail.com>
2019-03-12 12:09:22 +0100

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates :title,presence:true,length:{minimum:5,maximum:20}
   validates :body,presence:true,length:{minimum:20,maximum:360}
